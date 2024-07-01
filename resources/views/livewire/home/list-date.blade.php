770e83c409aac6acef196f0bc4c937339a55e4f6
Yapidev <88489723+Yapidev@users.noreply.github.com>
2024-07-01 18:12:40 +0700

    Update list-date.blade.php

--- resources/views/livewire/home/list-date.blade.php
+++ resources/views/livewire/home/list-date.blade.php
@@ -7,17 +7,28 @@
                     <h6 class="note-title text-truncate w-75 mb-0" data-noteHeading="Book a Ticket for Movie">
                         {{ $item->location }}</h6>
                     <p class="note-date fs-2">
-                        {{ \Carbon\Carbon::parse($item->date_time)->translatedFormat('d F Y') }}
+                        {{ \Carbon\Carbon::parse($item->date_time)->translatedFormat('l, d F Y') }}
                     </p>
                     <div class="note-content">
                         <p class="note-inner-content"
                             data-noteContent="Blandit tempus porttitor aasfs. Integer posuere erat a ante venenatis.">
                             {{ $item->description ? $item->description : 'Tidak ada deskripsi' }} </p>
                     </div>
-                    <div class="d-flex align-items-center">
-                        <a href="{{ route('date.show', $item->id) }}" class="btn btn-primary" wire:navigate>
-                            Lihat Detail
-                        </a>
+                    <div class="btn-group col-7">
+                        <a href="{{ route('date.show', $item->id) }}" class="btn btn-primary" wire:navigate>Lihat
+                            Detail</a>
+                        <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="sr-only">Toggle Dropdown</span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a class="dropdown-item cursor-pointer">Edit</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item cursor-pointer">Hapus</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
