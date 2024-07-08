e0f728745da791dc4f59ed47f388f4c4c7653b5b
Yapidev <88489723+Yapidev@users.noreply.github.com>
2024-07-08 21:22:45 +0700

    Update show.blade.php

--- resources/views/date/show.blade.php
+++ resources/views/date/show.blade.php
@@ -8,17 +8,17 @@
     <!-- Nav tabs -->
     <ul class="nav nav-pills flex-column flex-sm-row mt-4" role="tablist">
         <li class="nav-item flex-sm-fill text-sm-center">
-            <a class="nav-link active" data-bs-toggle="tab" href="#navpill-11" role="tab">
+            <a class="nav-link border border-primary active" data-bs-toggle="tab" href="#navpill-11" role="tab">
                 <span>Pengeluaran</span>
             </a>
         </li>
         <li class="nav-item flex-sm-fill text-sm-center">
-            <a class="nav-link" data-bs-toggle="tab" href="#navpill-22" role="tab">
+            <a class="nav-link border border-primary" data-bs-toggle="tab" href="#navpill-22" role="tab">
                 <span>Statistik</span>
             </a>
         </li>
         <li class="nav-item flex-sm-fill text-sm-center">
-            <a class="nav-link" data-bs-toggle="tab" href="#navpill-33" role="tab">
+            <a class="nav-link border border-primary" data-bs-toggle="tab" href="#navpill-33" role="tab">
                 <span>Review Date</span>
             </a>
         </li>
