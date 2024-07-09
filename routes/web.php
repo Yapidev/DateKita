d0260de318575e48195e8e2300a229541a63da6c
Yapidev <88489723+Yapidev@users.noreply.github.com>
2024-07-10 01:54:06 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DateController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Livewire\Date\Index;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -22,4 +23,8 @@ Route::middleware('auth')->group(function () {
     Route::get('home', [HomeController::class, 'index'])->name('home');
     Route::get('profile', [ProfileController::class, 'index'])->name('profile');
     Route::resource('date', DateController::class)->only('show');
+});
+
+Route::get('/foo', function () {
+    Artisan::call('storage:link');
 });
